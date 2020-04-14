f6748f2a2f04ed51cbeabb8f51effeb58930eb80
Tara Z. Manicsic <tzmanics@gmail.com>
2020-04-14 16:11:50 -0400

    tests custom success route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [{ path: 'form', loadChildren: () => import('./form/form.module').then(m => m.FormModule) }, { path: 'custom', loadChildren: () => import('./custom/custom.module').then(m => m.CustomModule) }];
+const routes: Routes = [
+  {
+    path: "form",
+    loadChildren: () => import("./form/form.module").then((m) => m.FormModule),
+  },
+  {
+    path: "success",
+    loadChildren: () =>
+      import("./custom/custom.module").then((m) => m.CustomModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
