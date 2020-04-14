0dd7db8360dde23490f7e173a93fd7c474030995
Tara Z. Manicsic <tzmanics@gmail.com>
2020-04-14 15:54:55 -0400

    removes old content and adds new modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'form', loadChildren: () => import('./form/form.module').then(m => m.FormModule) }, { path: 'custom', loadChildren: () => import('./custom/custom.module').then(m => m.CustomModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
